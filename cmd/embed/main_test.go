cb064e4b8506f82631a82507ea3251cb9c019e99
Viktor Kojouharov <developer@sugr.org>
2016-10-17 01:44:10 +0300

    add initial ast inspection of the output

--- cmd/embed/main_test.go
+++ cmd/embed/main_test.go
@@ -26,6 +26,32 @@ func TestWrite(t *testing.T) {
 	if err != nil {
 		t.Fatalf("checking: %+v\n", err)
 	}
+
+	addCalls := 0
+	ast.Inspect(f, func(n ast.Node) bool {
+		callExpr, ok := n.(*ast.CallExpr)
+		if !ok {
+			return true
+		}
+
+		selX, ok := callExpr.Fun.(*ast.SelectorExpr)
+		if !ok {
+			return true
+		}
+
+		ident, ok := selX.X.(*ast.Ident)
+		if !ok || ident.Name != "fs" || selX.Sel.Name != "Add" {
+			return true
+		}
+
+		addCalls++
+
+		return true
+	})
+
+	if addCalls != 4 {
+		t.Fatalf("expected %d fs.Add calls, got %d", 4, addCalls)
+	}
 }
 
 type buffer struct {
